83b1b24aabeee3a2d7049dbfe55ec748598e4390
bagtab <mrjohannisson@hotmail.com>
2019-06-04 06:25:53 +0200

    corrected exceptionlogger to write stacktrace to file

--- src/exceptions/ExceptionLogger.java
+++ src/exceptions/ExceptionLogger.java
@@ -1,23 +1,17 @@
 package exceptions;
 
 import java.io.File;
-import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class ExceptionLogger {
 	File errorLog;
-	FileWriter writer;
+	PrintWriter writer;
 
 	private ExceptionLogger() {
-		errorLog = new File("errorLog");
-		try {
-			if (!errorLog.exists()) {
-				errorLog.createNewFile();
-			}
-			writer = new FileWriter(errorLog);
-		} catch (IOException e) {
-			System.out.println("file errorLog couldnt be created or found, the error log will not work");
-		}
+		
 	}
 	private static class ExceptionLoggerHolder {
 		private static ExceptionLogger instance = new ExceptionLogger();
@@ -39,22 +33,13 @@ public class ExceptionLogger {
 	 */
 	public void writeToLog(String source, Exception exception) {
 		try {
-			writer.append("exception happened in" + source + " : " + exception.getMessage());
-		} catch (IOException newException) {
-			// don't know how to log an exception in the exception logger, left empty
-		} catch (NullPointerException newException) {
-			// error message already sent
-		}
-	}
-
-	/**
-	 * sends the exception log to the developer. not actually implemented.
-	 */
-	public void sendLogToDeveloper() {
-		try {
-			writer.close();
+			errorLog = new File("errorLog " + new SimpleDateFormat("yyyy-MM-dd HH-mm-ss").format(new Date()));
+			if(!errorLog.exists()) {
+				errorLog.createNewFile();				
+			}
+			exception.printStackTrace(writer);
 		} catch (IOException e) {
-			// don't know how to log an exception in the exception logger, left empty
+			System.out.println("file errorLog couldnt be created or found, the error log will not work");
 		}
 	}
 }
